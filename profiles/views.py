a0ddc4e721fd25c41529f410db53491cf8341e4d
mohammedegila <mohammedegila@gmail.com>
2021-05-01 00:30:49 +0200

    Update porofile list view & Add[add, watting and remove freind logic]

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Profile, Relationship
 from .forms import ProfileModelForm
-
+from django.views.generic import ListView
+from django.contrib.auth.models import  User
 # Create your views here.
 def my_profile_view(request):
 
@@ -45,3 +46,36 @@ def profiles_list_view(request):
     context = {'qs': qs}
 
     return render(request, 'profiles/profile_list.html', context)
+
+
+class ProfileListView(ListView):
+    model = Profile
+    template_name = 'profiles/profile_list.html'
+    # context_object_name = 'qs'
+
+    def get_queryset(self):
+        qs = Profile.objects.get_all_profiles(self.request.user)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = User.objects.get(username__iexact=self.request.user)
+        profile = Profile.objects.get(user=user)
+
+
+
+        rel_r = Relationship.objects.filter(sender=profile)
+        rel_s = Relationship.objects.filter(receiver=profile)
+        rel_receiver = []
+        rel_sender = []
+        for item in rel_r:
+            rel_receiver.append(item.receiver.user)
+        for item in rel_s:
+            rel_sender.append(item.sender.user)
+        context["rel_receiver"] = rel_receiver
+        context["rel_sender"] = rel_sender
+        context['is_empty'] = False
+        if len(self.get_queryset()) == 0:
+            context['is_empty'] = True
+
+        return context
