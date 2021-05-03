fed0653c4ebed6c8a986545d173a7a133e63812b
Ahmed-Saleh-007 <a85851a@gmail.com>
2021-05-03 05:08:57 +0200

    switch to mysql

--- socialBlog/settings.py
+++ socialBlog/settings.py
@@ -82,27 +82,27 @@ WSGI_APPLICATION = 'socialBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
-}
-
 #DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'OPTIONS': {
-#        'sql_mode': 'traditional',
-#        },
-#        'NAME': 'social_network',
-#        'USER': 'root',
-#        'PASSWORD':'',
-#        'HOST':'localhost',
-#        'PORT': 3306
-#
-#    }
+#     'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+        'sql_mode': 'traditional',
+        },
+        'NAME': 'social_network',
+        'USER': 'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT': 3306
+
+    }
+}
 
 
 # Password validation
