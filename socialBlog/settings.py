f5e89a2aed235682f7f7bb52aae43790cf850109
Bassam Saad <bassamsaad771@gmail.com>
2021-05-04 04:03:52 +0200

    fix

--- socialBlog/settings.py
+++ socialBlog/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'NAME': 'social_network',
         'USER': 'root',
         'PASSWORD':'',
-        'HOST':'localhost',
+        'HOST':'127.0.0.1',
         'PORT': 3306
 
     }
