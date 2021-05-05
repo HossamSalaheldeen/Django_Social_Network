eebda2f1da630f28efa877ef8f93fa9bd409b398
Bassam Saad <bassamsaad771@gmail.com>
2021-05-05 19:57:33 +0200

    fix

--- posts/forms.py
+++ posts/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 from .models import Post, Comment
-
+from groups.models import Group
+from flask import request
 class PostModelForm(forms.ModelForm):
     content = forms.CharField(widget=forms.Textarea(attrs={'rows':2}))
     class Meta:
         model = Post
         fields = ('content', 'group', 'image')
-
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 class CommentModelForm(forms.ModelForm):
     body = forms.CharField(label='', 
                             widget=forms.TextInput(attrs={'placeholder': 'Add a comment...'}))
